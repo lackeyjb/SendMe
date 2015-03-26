e6aae29e57cb480c42c5863d3e62837d48dd5068
Bryan Lackey <bryan.lackey@gmail.com>
2015-03-26 17:57:41 -0400

    adds nav bar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user_path @user
+      redirect_to user_path user
     else
       flash.now[:danger] = 'Invalid email/password combination.'
       render 'new'
@@ -17,6 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     sign_out
-    redirect_to root_path
+    redirect_to signin_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root 'users#new'
+  root 'sessions#new'
 
-  resources :users
+  resources :users, except: [:new]
+  resources :sessions, only: [:create, :destroy]
   resources :text_messages
   post '/success', to: 'text_messages#send_message', as: :text_success
+  get '/signin',        to: 'sessions#new'
+  delete '/signout',    to: 'sessions#destroy'
+  get '/signup',        to: 'users#new', as: :signup
 end
