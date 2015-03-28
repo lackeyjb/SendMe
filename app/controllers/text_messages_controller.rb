d3019e11732ef015ea50543cc7d5f564809369b5
Bryan Lackey <bryan.lackey@gmail.com>
2015-03-28 13:51:03 -0400

    moves methods from text_message controller to text_message model

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -1,16 +1,6 @@
 class TextMessagesController < ApplicationController
 
-  VALID_PHONE_NUMBER_REGEX = /^(\+0?1\s)?\(?\d{3}\)?[\s.]\d{3}[\s.]\d{4}$/
-
   def send_message
-    # if params[:phone_number] == VALID_PHONE_NUMBER_REGEX.match("#{params[:phone_number]}")
-      account_sid  = 'AC01b73dc19e96d27b4a493086460799b7'
-      auth_token   = '3eceeae4edd02236003b89b0443765a0'
-      @client = Twilio::REST::Client.new account_sid, auth_token
-      @client.messages.create(
-        body: "#{current_user.name} \n #{current_user.email} \n #{current_user.addition_info}",
-        to:   "+1" + params[:text_messages][:phone_number],
-        from: "+16789168205")
-    # end
+    TextMessage.send_user_info(current_user, params[:text_messages][:phone_number])
   end
 end
--- app/models/text_message.rb
+++ app/models/text_message.rb
@@ -1,14 +1,21 @@
-include ActionView::Helpers
-
 class TextMessage
 
-  
+  VALID_PHONE_NUMBER_REGEX = /^[0-9]+$/
 
-  # def self.send_user_info(phone_number) 
-    
-  # end
+  def self.configure_twilio
+    account_sid  = 'AC01b73dc19e96d27b4a493086460799b7'
+    auth_token   = '3eceeae4edd02236003b89b0443765a0'
+    Twilio::REST::Client.new account_sid, auth_token
+  end
 
-  # def self.valid_phone_number?(phone_number)
-  #   phone_number == VALID_PHONE_NUMBER_REGEX.match("#{phone_number}")
-  # end
+  def self.send_user_info(user, phone_number) 
+    configure_twilio.messages.create(
+      body: "#{user.name} \n #{user.email} \n #{user.addition_info}",
+      to:   "+1" + phone_number,
+      from: "+16789168205") if valid_phone_number?(phone_number)
+  end
+
+  def self.valid_phone_number?(phone_number)
+    phone_number == VALID_PHONE_NUMBER_REGEX.match(phone_number).to_s
+  end
 end
